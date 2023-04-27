c8b442ca8506b08c05315b76a50c816451c5312e
Sam Mercer <smercerds@gmail.com>
2023-04-27 12:22:45 +0100

    feat(UI): style auth modals, add hero pattern

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,6 +20,26 @@ export default function App({ Component, pageProps }) {
             <ClerkProvider
                 appearance={{
                     baseTheme: dark,
+                    elements: {
+                        rootBox: {
+                            fontFamily: 'Inconsolata, monospace',
+                        },
+                        card: 'bg-base',
+                        headerTitle: 'text-white opacity-het',
+                        headerSubtitle: 'text-white opacity-met',
+                        modalCloseButton:
+                            'text-tertiary text-opacity-het hover:text-opacity-100 bg-transparent hover:bg-transparent',
+                        formFieldLabel: 'text-white opacity-het',
+                        formFieldInput:
+                            'bg-white bg-opacity-e1 border-2 border-white border-opacity-e5 text-opacity-met focus:ring-0 focus:border-opacity-met',
+                        formFieldInputShowPasswordButton:
+                            'text-white opacity-e6 hover:text-white hover:opacity-e8 focus:ring-0',
+                        formButtonPrimary:
+                            'bg-primary hover:bg-primary bg-opacity-het hover:bg-opacity-100 text-black text-opacity-het capitalize',
+                        footerActionText: 'font-body text-white opacity-met',
+                        footerActionLink:
+                            'font-body text-secondary hover:text-secondary opacity-het hover:opacity-100 hover:no-underline',
+                    },
                 }}
                 {...pageProps}
             >
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,29 +5,29 @@ export default function Home() {
     const { isLoaded: userLoaded, isSignedIn } = useUser()
     const user = useUser()
     if (!userLoaded) {
-        // handle loading state here
+        // Handle loading state here
         return null
     }
     return (
-        <main className="font-body font-semibold h-screen w-screen bg-base">
+        <main className="font-body font-semibold h-screen w-screen bg-base bg-hero-pattern">
             <Navbar isSignedIn={isSignedIn} />
             <div className="mt-20 flex flex-col justify-center items-center">
-                <h1 className="flex font-extrabold md:text-8xl sm:text-6xl text-4xl font-heading text-transparent bg-clip-text bg-gradient-to-r from-primary via-secondary to-tertiary background-animate animate-pulse">
+                <h1 className="flex font-extrabold md:text-8xl sm:text-7xl text-6xl font-heading text-transparent bg-clip-text bg-gradient-to-r from-primary via-secondary to-tertiary animate-title">
                     deeptune.ai
                 </h1>
-                <h2 className="font-extrabold p-6 mb-16 md:text-4xl sm:text-2xl text-l text-white text-opacity-met text-center">
+                <h2 className="font-extrabold p-4 mb-16 md:text-3xl sm:text-2xl text-xl text-white text-opacity-met text-center">
                     The Home of AI Music &#129302; &#127926;
                 </h2>
-                <div className="bg-white bg-opacity-e2 w-screen p-8">
-                    <h2 className="font-extrabold md:text-2xl sm:text-xl text-white text-opacity-het text-center">
+                <div className="bg-base w-screen py-8 px-16">
+                    <h2 className="font-extrabold md:text-2xl sm:text-xl text-lg text-white text-opacity-het text-center">
                         Create and Discover AI Covers From Your Favorite
                         Celebrities
                     </h2>
                     <div className="flex flex-row justify-center items-center mt-8 space-x-8">
-                        <button className="self-center w-56 py-5 px-2 rounded bg-secondary bg-opacity-het hover:bg-opacity-100 text-black md:text-xl sm:text-l text-xs text-opacity-het">
+                        <button className="self-center w-56 py-5 px-2 rounded bg-secondary bg-opacity-het hover:bg-opacity-100 text-black md:text-xl sm:text-lg text-xs text-opacity-het">
                             Create &#127908;
                         </button>
-                        <button className="self-center w-56 py-5 px-2 rounded bg-tertiary bg-opacity-het hover:bg-opacity-100 text-black md:text-xl sm:text-l text-xs text-opacity-het">
+                        <button className="self-center w-56 py-5 px-2 rounded bg-tertiary bg-opacity-het hover:bg-opacity-100 text-black md:text-xl sm:text-lg text-xs text-opacity-het">
                             Discover &#127911;
                         </button>
                     </div>
